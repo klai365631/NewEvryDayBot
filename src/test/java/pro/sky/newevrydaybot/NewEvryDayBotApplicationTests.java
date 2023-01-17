2acdf1f2fdf20021f24f69ce52d53c5ff0e7f2da
ANDRE <klai1@mail.ru>
2023-01-17 16:54:22 +0400

    Initial commit

---
+++ src/test/java/pro/sky/newevrydaybot/NewEvryDayBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.newevrydaybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewEvryDayBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
